448d6a0755dd31cb14c339bfe7e3661ddd037715
Gísia <gisia.luan@gmail.com>
2026-03-23 19:57:41 -0300

    Adiciona arquivos

---
+++ src/main/java/com/gisia/notificacao/businnes/EmailService.java
@@ -0,0 +1,47 @@
+package com.gisia.notificacao.businnes;
+
+import com.gisia.notificacao.businnes.dto.TarefasDTO;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.nio.charset.StandardCharsets;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
+
+    @Value("${envio.email.remetente}")
+    public String remetente;
+
+    @Value("${envio.email.nomeRemetente}")
+    private final String nomeRemetente;
+
+
+    public void enviaEmail(TarefasDTO dto) {
+
+        try {
+            MimeMessage mensagem = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mensagem, true, StandardCharsets.UTF_8.name());
+
+
+            mimeMessageHelper.setFrom(new InternetAddress(remetente, nomeRemetente));
+            mimeMessageHelper.setTo(InternetAddress.parse(dto.getEmailUsuario()));
+            mimeMessageHelper.setSubject("Notificação de Tarefa");
+
+            Context context = new Context();
+
+
+        }
+
+    }
+}
